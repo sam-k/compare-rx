870aab5b205006ac8ace17e3daf17f320d74cb92
Sam K <samkim1729@gmail.com>
2019-02-02 09:22:47 -0500

    Initialize project

---
+++ compare_drug_prices.R
@@ -0,0 +1,15 @@
+# ----------------------------------------- #
+#             THInC CompareRx               #
+#             Drug Price Data               #
+#             February 2, 2018              #
+#                  Team 29                  #
+# ----------------------------------------- #
+
+#### ------------------ load packages ------------------ ####
+library(readr)
+library(dplyr)
+library(tidyr)
+library(magrittr)
+
+
+#### ------------------- read in data ------------------ ####
